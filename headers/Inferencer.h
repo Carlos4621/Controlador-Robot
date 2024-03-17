f7ba28e0d6de51e2580781caf352ef41ee52bae2
Carlos Ivan <carlosivanhb@hotmail.com>
2024-03-17 16:45:50 -0600

    Actualizado y mejorado el soporte para la detección de hazmat labels

---
+++ headers/Inferencer.h
@@ -0,0 +1,61 @@
+#pragma once
+#ifndef INFERENCER_HEADER
+#define INFERENCER_HEADER
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <opencv2/opencv.hpp>
+
+namespace My {
+
+    /// @brief Struct que contiene los datos de las detecciones
+    struct InferenceData {
+        uint16_t classID;
+        std::string className;
+        double confidence;
+        cv::Rect bouncingBox;
+    };
+
+    /// @brief Clase que permite el uso de modelos YOLOv8 exportados a .onnx
+    class Inferencer {
+    public:
+
+        /// @brief Constructor base
+        /// @param onnxModelPath Ruta al modelo .onnx
+        /// @param modelInputShape Tamaño de las imágenes de entrada del modelo
+        /// @param classNamesPath Ruta a los nombres de las classes dependiendo su ID
+        /// @param runWithCuda Decidir si usar CUDA. Default = true
+        Inferencer(std::string_view onnxModelPath, const cv::Size& modelInputShape, std::string_view classNamesPath, const bool& runWithCuda = true);
+
+        Inferencer(const Inferencer&) = default;
+        Inferencer(Inferencer&&) noexcept = default;
+
+        virtual ~Inferencer() noexcept = default;
+
+        Inferencer& operator=(const Inferencer&) = default;
+        Inferencer& operator=(Inferencer&&) noexcept = default;
+
+        /// @brief Aplica el modelo a la imagen proporcionada y devuelve las inferencias
+        /// @param inputImage Imagen a usar
+        /// @param modelConfidenceThreshold Threshold de la confidencia
+        /// @param modelScoreThreshold Threshold del score
+        /// @param modelNMSThreshold Threshold del NMS
+        /// @return Vector con los datos de las inferencias
+        [[nodiscard]] std::vector<InferenceData> getInferences(const cv::Mat& inputImage, const double& modelConfidenceThreshold = 0.25f,
+            const double& modelScoreThreshold = 0.45f, const double& modelNMSThreshold = 0.5f);
+
+    private:
+
+        std::vector<std::string> classNames_m;
+
+        const cv::Size2f modelInputSize_m;
+
+        cv::dnn::Net network_m;
+
+        void loadClasses(std::string_view path);
+        void loadOnnxNetwork(std::string_view path, const bool& cudaEnabled);
+    };
+}
+#endif // INFERENCE_H
--- inferenceTest.cpp
+++ inferenceTest.cpp
@@ -1,70 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <getopt.h>
-
-#include <opencv2/opencv.hpp>
-
-#include "inference.h"
+#include "Inferencer.h"
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char **argv)
-{
-    std::string projectBasePath = "/home/carlos4621/ultralytics"; // Set your ultralytics base path
+int main() {
+    
+    const std::string modelPath{ "/home/carlos4621/Escritorio/Controlador-Robot/resources/hazmatModel.onnx" };
+    const std::string classesPath{ "/home/carlos4621/Escritorio/Controlador-Robot/resources/hazmatClasses.txt" };
 
-    bool runOnGPU = false;
-
-    //
-    // Pass in either:
-    //
-    // "yolov8s.onnx" or "yolov5s.onnx"
-    //
-    // To run Inference with yolov8/yolov5 (ONNX)
-    //
-
-    // Note that in this example the classes are hard-coded and 'classes.txt' is a place holder.
-    Inference inf(projectBasePath + "/best.onnx", cv::Size(640, 640), "classes.txt", runOnGPU);
+    My::Inferencer inf{ modelPath, cv::Size(640, 640), classesPath, false };
 
     std::vector<std::string> imageNames;
-    imageNames.push_back(projectBasePath + "/ultralytics/assets/test1.jpg");
-    imageNames.push_back(projectBasePath + "/ultralytics/assets/test2.jpg");
+    imageNames.emplace_back("/home/carlos4621/Escritorio/Controlador-Robot/resources/hazmatTest1.jpg");
+    imageNames.emplace_back("/home/carlos4621/Escritorio/Controlador-Robot/resources/hazmatTest2.jpg");
+    imageNames.emplace_back("/home/carlos4621/Escritorio/Controlador-Robot/resources/nothing.jpg");
 
-    for (int i = 0; i < imageNames.size(); ++i)
-    {
-        cv::Mat frame = cv::imread(imageNames[i]);
+    cv::Scalar boxColor{ 255, 0, 255 };
+    for (const auto& i : imageNames) {
 
-        // Inference starts here...
-        std::vector<Detection> output = inf.runInference(frame);
+        auto frame{ cv::imread(i) };
 
-        int detections = output.size();
-        std::cout << "Number of detections:" << detections << std::endl;
+        auto inferences{ inf.getInferences(frame) };
 
-        for (int i = 0; i < detections; ++i)
-        {
-            Detection detection = output[i];
+        std::cout << "Detecciones:" << inferences.size() << std::endl;
 
-            cv::Rect box = detection.box;
-            cv::Scalar color = detection.color;
+        for (const auto& j : inferences) {
 
-            // Detection box
-            cv::rectangle(frame, box, color, 2);
+            cv::rectangle(frame, j.bouncingBox, boxColor, 2);
 
-            // Detection box text
-            std::string classString = detection.className + ' ' + std::to_string(detection.confidence).substr(0, 4);
-            cv::Size textSize = cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-            cv::Rect textBox(box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
+            const auto classString{ j.className + ' ' + std::to_string(j.confidence).substr(0, 4) };
+            const auto textSize{ cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0) };
+            cv::Rect textBox{ j.bouncingBox.x, j.bouncingBox.y - 40, textSize.width + 10, textSize.height + 20 };
 
-            cv::rectangle(frame, textBox, color, cv::FILLED);
-            cv::putText(frame, classString, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
+            cv::rectangle(frame, textBox, boxColor, cv::FILLED);
+            cv::putText(frame, classString, cv::Point{ j.bouncingBox.x + 5, j.bouncingBox.y - 10 }, cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar{ 0, 0, 0 }, 2, 0);
         }
-        // Inference ends here...
 
-        // This is only for preview purposes
-        float scale = 0.8;
-        cv::resize(frame, frame, cv::Size(frame.cols*scale, frame.rows*scale));
         cv::imshow("Inference", frame);
 
-        cv::waitKey(-1);
+        cv::waitKey();
     }
 }
