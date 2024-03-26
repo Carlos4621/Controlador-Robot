a8374516e33bcf424a460e0e50c3542bdd1e6642
Carlos Ivan <carlosivanhb@hotmail.com>
2024-03-26 08:09:59 -0600

    Añadido un giro de 90° primitivo

--- sources/CarController.cpp
+++ sources/CarController.cpp
@@ -8,6 +8,14 @@ boost::asio::io_context &ioContext, const HW039ControllerParams &rightMotor,
 
 // TODO: Mejorar esta cosa
 void My::CarController::applyChanges() {
+  const auto controllerData{getControllerData()};
+
+  if (controllerData.leftStick.first > 5 || controllerData.leftStick.first < -5) {
     motors_m.first.setRelative(getControllerData().leftStick.first);
     motors_m.second.setRelative(getControllerData().leftStick.first);
+  }
+  else if (controllerData.leftStick.second > 5 || controllerData.leftStick.second < -5) {
+    motors_m.first.setRelative(controllerData.leftStick.second * (controllerData.leftStick.second < -5 ? -1 : 1));
+    motors_m.second.setRelative(controllerData.leftStick.second * (controllerData.leftStick.second > 5 ? -1 : 1));
+  }
 }
